8d783789c587008e64708fd3f3654089fa72f8b1
BMDan <github@angelfaq.com>
2015-03-06 18:01:50 -0500

    Optional parameter: command to be run.  Otherwise, provide a pretty-printed rundown of uptime, load averages, and free RAM.

--- sysload.sh
+++ sysload.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-nova-manage service list | awk '$3=="nova" {print $2}' | sed 's/ /\n/g' | xargs -n 1 -P 255 -I '{}' sudo -u nova ssh {} 'echo $(hostname)" "$(uptime) $(free -m | awk "NR==3")' | sort
+DEF_RMTCMD='printf "%-24s %-44s %20s %s\\n" "$(hostname)" "$(uptime | sed "s/,[ ]*load average:.*//")" "$(uptime | awk -F: "{print \$NF}")" "$(free -m | awk "NR==3 {print \"Free: \" \$4 \" MB\"}")"'
+RMTCMD=${1:-$DEF_RMTCMD}
+
+nova-manage service list | awk '$3=="nova" {print $2}' | sed 's/ /\n/g' | xargs -n 1 -P 255 -I '{}' sudo -u nova ssh {} "$RMTCMD" | sort
