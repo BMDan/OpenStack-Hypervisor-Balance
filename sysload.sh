127afaad3823fb0e27849818a7198ebfa3d8a9b9
BMDan <github@angelfaq.com>
2015-03-26 16:06:43 -0400

    Updating "test" (aka []) arguments to tolerate older versions of bash a bit better.

--- sysload.sh
+++ sysload.sh
@@ -3,7 +3,7 @@
 DEF_RMTCMD='printf "%-24s %-44s %20s %s\\n" "$(hostname)" "$(uptime | sed "s/,[ ]*load average:.*//")" "$(uptime | awk -F: "{print \$NF}")" "$(free -m | awk "NR==3 {print \"Free: \" \$4 \" MB\"}")"'
 RMTCMD=${1:-$DEF_RMTCMD}
 
-if [ "$1" == "-h" -o "$1" == "--help" ]; then
+if [ x"$1" == x"-h" -o x"$1" == x"--help" ]; then
   echo "Usage: $0 [command]"
   echo "  Note: In most cases, you probably want to use 'echo' and command substitution, so you'll know where"
   echo "        your output is coming from by using something like 'echo \$(hostname) \$(uptime\)'."
